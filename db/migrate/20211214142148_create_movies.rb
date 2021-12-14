f18834a2133c645d3b15efe69ff815f083f53f0a
camille breillot <camille.breillot@gmail.com>
2021-12-14 15:27:28 +0100

    model-movie-created-and-validations

---
+++ db/migrate/20211214142148_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :overview
+      t.string :synopsis
+      t.float :rating
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
