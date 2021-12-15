928d18f6e730739b62d4e4a842d03f438f608e80
camille breillot <camille.breillot@gmail.com>
2021-12-15 10:27:18 +0100

    review model and associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true, presence: true
   has_many :lists
+  has_many :reviews
 end
