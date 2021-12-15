864cb05bd29caca3a0849b575c6431473374f7a0
camille breillot <camille.breillot@gmail.com>
2021-12-15 15:03:08 +0100

    features index show new create liste

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    @list.status = false
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render "lists/index"
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_135851) do
+ActiveRecord::Schema.define(version: 2021_12_15_102626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "synopsis"
+    t.float "rating"
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "poster_url"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.float "rating"
+    t.string "comment"
+    t.bigint "list_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_reviews_on_list_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +69,9 @@ ActiveRecord::Schema.define(version: 2021_12_14_135851) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "lists", "users"
+  add_foreign_key "reviews", "lists"
+  add_foreign_key "reviews", "users"
 end
