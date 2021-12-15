f8c88d1a13a587d56ea8b0a7d0f2bb264c4009cc
camille breillot <camille.breillot@gmail.com>
2021-12-15 16:18:42 +0100

    index, show, create methods created for bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
