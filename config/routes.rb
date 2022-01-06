4487cb1dcfd48de4b02a379ed391986cac57c21d
camille breillot <camille.breillot@gmail.com>
2022-01-06 11:07:49 +0100

    migration done and photo changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'lists#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :index, :show]
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ results_serialized = URI.open(url).read
 results = JSON.parse(results_serialized)
 movies = results["results"]
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"], second_poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}")
 end
 
 url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4aa434daa023809deee08630db596914&language=en-US&page=2'
@@ -28,7 +28,7 @@ results_serialized = URI.open(url).read
 results = JSON.parse(results_serialized)
 movies = results["results"]
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"], second_poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}")
 end
 
 url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4aa434daa023809deee08630db596914&language=en-US&page=3'
@@ -36,7 +36,7 @@ results_serialized = URI.open(url).read
 results = JSON.parse(results_serialized)
 movies = results["results"]
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"], second_poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}")
 end
 
 url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4aa434daa023809deee08630db596914&language=en-US&page=4'
@@ -44,7 +44,7 @@ results_serialized = URI.open(url).read
 results = JSON.parse(results_serialized)
 movies = results["results"]
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"], second_poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}")
 end
 
 url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4aa434daa023809deee08630db596914&language=en-US&page=5'
@@ -52,7 +52,7 @@ results_serialized = URI.open(url).read
 results = JSON.parse(results_serialized)
 movies = results["results"]
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"], second_poster_url: "https://image.tmdb.org/t/p/w500#{movie["backdrop_path"]}")
 end
 
 puts "Creating users"
@@ -60,9 +60,13 @@ camille = User.create(email: "camille@gmail.com", username: "Camille", password:
 User.create(email: "pierrelouis@gmail.com", username: "Pierre-Louis", password: "password")
 
 puts "Creating Lists"
-List.create(name: "Classic movies", user_id: camille.id)
-List.create(name: "Superhero", user_id: camille.id)
-List.create(name: "Drama", user_id: camille.id)
-List.create(name: "Nineties", user_id: camille.id)
+list1 = List.create(name: "Classic movies", user_id: camille.id)
+list1.photo.attach(io: File.open('app/assets/images/classic.jpeg'), filename: 'classic.jpeg')
+list2 = List.create(name: "Superhero", user_id: camille.id)
+list2.photo.attach(io: File.open('app/assets/images/superhero.jpeg'), filename: 'superhero.jpeg')
+list3 = List.create(name: "Drama", user_id: camille.id)
+list3.photo.attach(io: File.open('app/assets/images/drama.jpeg'), filename: 'drama.jpeg')
+list4 = List.create(name: "Nineties", user_id: camille.id)
+list4.photo.attach(io: File.open('app/assets/images/nineties.jpeg'), filename: 'nineties.jpeg')
 
 puts "Seed created"
