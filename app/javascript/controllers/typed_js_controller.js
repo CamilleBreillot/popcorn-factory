deafc9865d531f06e0fe34e986c4f3216982adfc
camille breillot <camille.breillot@gmail.com>
2022-01-06 10:35:40 +0100

    banner-show-modified

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,7 @@ import Typed from "typed.js";
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ['Save all your movie in one place ^1000', 'Save all your movie anytime'],
+      strings: ['Save all your movie anytime^1000', 'Save all your movie in one place'],
       smartBackspace: true ,// Default value
       typeSpeed: 50
     });
