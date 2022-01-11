e4320c2eeef9030d9d6068ee6f50406538d941dc
camille breillot <camille.breillot@gmail.com>
2022-01-11 16:12:00 +0100

    review controller new and create actions + pundit for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    authorize @review
   end
 
   def create
@@ -9,6 +10,7 @@ class ReviewsController < ApplicationController
     @review.user = current_user
     @list = List.find(params[:list_id])
     @review.list = @list
+    authorize @review
     if @review.save
       redirect_to list_path(@list)
     else
